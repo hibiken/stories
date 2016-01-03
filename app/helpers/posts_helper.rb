ede41300f008bdfb362ce67153f6916950514a58
Ken Hibino <ken.hibino7@gmail.com>
2016-01-03 16:22:03 +0900

    Improve posts_helper_spec

--- app/helpers/posts_helper.rb
+++ app/helpers/posts_helper.rb
@@ -2,7 +2,7 @@ module PostsHelper
   def post_length_in_minutes(body)
     min = body.split(" ").size / 250
     if min == 0
-      'less than a minute'
+      'less than a minute read'
     else
       "#{min} min read"
     end
--- spec/helpers/posts_helper_spec.rb
+++ spec/helpers/posts_helper_spec.rb
@@ -3,19 +3,16 @@ require "rails_helper"
 RSpec.describe PostsHelper do
 
   describe "#post_length_in_minutes" do
-    let(:post) { build_stubbed(:post) }
-
     it "calculates the time to read an article" do
       five_hundred_words = "word " * 500
-      post.body = five_hundred_words
-      actual = helper.post_length_in_minutes(post.body)
+      actual = helper.post_length_in_minutes(five_hundred_words)
       expect(actual).to eq("2 min read")
     end
 
     it "can handle really short post" do
-      post.body = "word " * 90
-      actual = helper.post_length_in_minutes(post.body)
-      expect(actual).to eq("less than a minute")
+      less_than_one_hundred = "word " * 90
+      actual = helper.post_length_in_minutes(less_than_one_hundred)
+      expect(actual).to eq("less than a minute read")
     end
   end
 end
