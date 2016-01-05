7b443d902cc8afcb81840e7b1f9b251c545fb3df
Ken Hibino <ken.hibino7@gmail.com>
2016-01-05 14:13:58 +0900

    Add follow_button_for helper to refactor the view

--- app/helpers/application_helper.rb
+++ app/helpers/application_helper.rb
@@ -1,2 +1,16 @@
 module ApplicationHelper
+
+  def follow_button_for(user)
+    if user_signed_in?
+      unless current_user?(user)
+        if current_user.following?(user)
+          render 'shared/unfollow_button'
+        else
+          render 'shared/follow_button'
+        end
+      end
+    else
+      render 'shared/follow_button'
+    end
+  end
 end
