b9bbcda754219fa07390762136a79c3e12d0f367
Ken Hibino <ken.hibino7@gmail.com>
2016-01-06 10:23:34 +0900

    Add follow button for tags in tags#show page

--- app/controllers/interests_controller.rb
+++ app/controllers/interests_controller.rb
@@ -1,6 +1,15 @@
 class InterestsController < ApplicationController
+  before_action :authenticate_user!
 
   def create
+    tag = Tag.find(params[:tag_id])
+    current_user.follow_tag(tag)
+    redirect_to tag
+  end
 
+  def destroy
+    tag = Interest.find(params[:id]).tag
+    current_user.unfollow_tag(tag)
+    redirect_to tag
   end
 end
