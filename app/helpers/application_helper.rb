28ee955c00a7a53258c00374ef94e6b7f2fd8730
Ken Hibino <ken.hibino7@gmail.com>
2016-01-06 11:05:09 +0900

    Add follow_tag_button_for view helper to clean up the view in tags#show page

--- app/helpers/application_helper.rb
+++ app/helpers/application_helper.rb
@@ -13,4 +13,16 @@ module ApplicationHelper
       render 'shared/follow_button'
     end
   end
+
+  def follow_tag_button_for(tag)
+    if user_signed_in?
+      if current_user.following_tag?(tag)
+        render 'shared/unfollow_tag_button'
+      else
+        render 'shared/follow_tag_button'
+      end
+    else
+      render 'shared/follow_tag_button'
+    end
+  end
 end
