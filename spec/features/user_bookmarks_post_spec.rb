5ccbbb8bd162889b5abd752b54f73571c0aa3a61
Ken Hibino <ken.hibino7@gmail.com>
2016-01-08 10:10:37 +0900

    Add spec for bookmarking a post

---
+++ spec/features/user_bookmarks_post_spec.rb
@@ -0,0 +1,18 @@
+require "rails_helper"
+
+RSpec.feature "Bookmarking a post" do
+  let(:user) { create(:user) }
+  let(:post) { create(:post) }
+
+  scenario "signed-in user bookmarks a post successfully" do
+    sign_in user
+    visit post_path(post)
+    click_on "Bookmark"
+    expect(current_path).to eq(post_path(post))
+    expect(page).to have_button "Unbookmark"
+
+    click_on "Unbookmark"
+    expect(current_path).to eq(post_path(post))
+    expect(page).to have_button "Bookmark"
+  end
+end
