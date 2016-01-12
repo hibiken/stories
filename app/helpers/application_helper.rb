cf63ac55080b874a7ba22011b23b84ac72094804
Ken Hibino <ken.hibino7@gmail.com>
2016-01-12 23:01:56 +0900

    Refactor view for feature/unfeature tag buttons

--- app/helpers/application_helper.rb
+++ app/helpers/application_helper.rb
@@ -26,6 +26,14 @@ module ApplicationHelper
     end
   end
 
+  def feature_tag_button_for(tag)
+    if tag.featured?
+      render partial: 'admin/unfeature_tag_button', locals: { tag: tag }
+    else
+      render partial: 'admin/feature_tag_button', locals: { tag: tag }
+    end
+  end
+
   def nav_link_to(text, url, options = {})
     options[:class] ||= ""
     options[:class] += " active" if current_page?(url)
