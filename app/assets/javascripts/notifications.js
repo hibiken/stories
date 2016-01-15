4603a7850cbbec2b05b24658073678aa051fdafe
Ken Hibino <ken.hibino7@gmail.com>
2016-01-15 11:38:46 +0900

    Clean up notifications.js file

--- app/assets/javascripts/notifications.js
+++ app/assets/javascripts/notifications.js
@@ -1,13 +1,22 @@
-function notificationsInitialize() {
+$unreadCount = $('<span id="unread-notifications-count"></span>');
 
-  $notifications = $('#notifications');
-  $dropdown = $('#notification-items');
-  $bell = $('#bell');
 
-  $unreadCount = $('<span id="unread-notifications-count"></span>');
-  
+var Notification = {
 
-  var renderNotifications = function(data) {
+  setup: function() {
+    if ($('#notifications').length > 0) {
+      $.ajax({
+        url: "/api/notifications.json",
+        dataType: "JSON",
+        method: "GET",
+        success: Notification.render
+      });
+    }
+
+    $('#notifications').click(Notification.markAsRead);
+  },
+
+  render: function(data) {
     console.log(data);
     var itemContent, imageTag;
 
@@ -28,45 +37,31 @@ function notificationsInitialize() {
       return '<li><a href="' + notification.url + '">' + imageTag + itemContent + '</br><small>' + notification.time_ago + '</small></a></li>';
     });
 
-
     if (items.length > 0) {
-      $dropdown.html(items.join(' '));
+      $('#notification-items').html(items.join(' '));
       $unreadCount.text(items.length);
-      $bell.after($unreadCount);
-      $bell.hide();
-      $notifications.addClass('active');
+      $('#bell').after($unreadCount);
+      $('#bell').hide();
+      $('#notifications').addClass('active');
     } else {
-      $dropdown.html("<li><a>No notifications yet</a></li>");
+      $('#notification-items').html("<li><a>No notifications yet</a></li>");
     }
-  };
+  },
 
-  var markAsRead = function() {
+  markAsRead: function() {
     $.ajax({
       url: '/api/notifications/mark_as_read',
       method: 'POST',
       dataType: 'JSON',
       success: function() {
-        $bell.show();
-        $notifications.removeClass('active');
+        $('#bell').show();
+        $('#notifications').removeClass('active');
         $unreadCount.hide();
       }
-    });
-  };
-
-  /** Click handler to clear notifications **/
-  $notifications.click(markAsRead);
-
-  
-  if ($notifications.length > 0) {
-    $.ajax({
-      url: "/api/notifications.json",
-      dataType: "JSON",
-      method: "GET",
-      success: renderNotifications
     });
   }
 }
 
 
-$(document).ready( notificationsInitialize );
-$(document).on( 'page:load', notificationsInitialize );
+$(document).ready( Notification.setup );
+$(document).on( 'page:load', Notification.setup );
