7611fe518df7c042d43da606f778f9379e407595
Ken Hibino <ken.hibino7@gmail.com>
2016-01-19 11:16:15 +0900

    Make search URL clean

--- app/controllers/search_controller.rb
+++ app/controllers/search_controller.rb
@@ -1,6 +1,16 @@
 class SearchController < ApplicationController
+  before_action :beautify_url
+
   def show
-    @posts = Post.search(params[:search][:q]).records.to_a
-    @users = User.search(params[:search][:q]).records.to_a
+    @posts = Post.search(params[:q]).records.to_a
+    @users = User.search(params[:q]).records.to_a
   end
+
+  private
+
+    def beautify_url
+      if params[:search].present?
+        redirect_to search_url(q: params[:search][:q])
+      end
+    end
 end
