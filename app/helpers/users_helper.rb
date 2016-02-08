df8cf6b8b7ef4dbdd27fbe2d8622e3d5e6ff42a5
Ken Hibino <ken.hibino7@gmail.com>
2016-02-08 18:24:53 -0500

    Add CSS styles to Daily Digest email

--- app/helpers/users_helper.rb
+++ app/helpers/users_helper.rb
@@ -1,14 +1,15 @@
 module UsersHelper
   def avatar_for(user, options = { size: 80 })
     size = options[:size]
+    style = options[:style]
     if user.avatar?
       if size <= 80
-        image_tag user.avatar.url(:thumb), width: size, height: size, alt: user.username, class: 'avatar-image'
+        image_tag user.avatar.url(:thumb), width: size, height: size, alt: user.username, class: 'avatar-image', style: style
       else
-        image_tag user.avatar.url, width: size, height: size, alt: user.username, class: 'avatar-image'
+        image_tag user.avatar.url, width: size, height: size, alt: user.username, class: 'avatar-image', style: style
       end
     else
-      image_tag 'default-avatar.jpg', width: size, height: size, alt: 'avatar image', class: 'avatar-image'
+      image_tag 'default-avatar.jpg', width: size, height: size, alt: 'avatar image', class: 'avatar-image', style: style
     end
   end
 end
