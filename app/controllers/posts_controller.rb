718c6c9f39622368ed357c8179c24e3a27d8f7f2
Ken Hibino <ken.hibino7@gmail.com>
2016-02-10 16:03:41 -0500

    Add CSS styles to editor form

--- app/controllers/posts_controller.rb
+++ app/controllers/posts_controller.rb
@@ -12,8 +12,8 @@ class PostsController < ApplicationController
   end
 
   def create
-    post = current_user.posts.build(post_params)
-    if post.save
+    @post = current_user.posts.build(post_params)
+    if @post.save
       redirect_to root_url, notice: "Successfully created a post!"
     else
       flash.now[:alert] = "Could not save the post, Please try again"
