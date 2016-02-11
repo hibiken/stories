52bcf370f8fa73c88128dd24d744adce5f16566a
Ken Hibino <ken.hibino7@gmail.com>
2016-02-11 05:37:03 -0500

    Fix syntax error in welcome email job

--- app/jobs/welcome_email_job.rb
+++ app/jobs/welcome_email_job.rb
@@ -2,7 +2,7 @@ class WelcomeEmailJob < ActiveJob::Base
   queue_as :mailer
 
   def perform(user_id)
-    user = User.find_by(user_id)
+    user = User.find_by(id: user_id)
     UserMailer.welcome_email(user).deliver_now if user
   end
 end
