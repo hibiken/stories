d4c89d5a185c1d5f277fd0c3b1d8b5e2accb6052
Ken Hibino <ken.hibino7@gmail.com>
2016-02-15 19:07:12 -0500

    Add CSS animation for loader

--- app/assets/javascripts/endless_scroll.js
+++ app/assets/javascripts/endless_scroll.js
@@ -1,10 +1,12 @@
+$loading = $('<div class="loader-container"><div class="loader"><div class="loader-dot"></div><div class="loader-dot"></div><div class="loader-dot"></div><div class="loader-dot"></div><div class="loader-dot"></div><div class="loader-dot"></div><div class="loader-text"></div></div></div>');
+
 var EndlessScroll = {
   setup: function() {
     if ($('[data-behavior="endless-scroll"]').length > 0) {
       $(window).scroll(function() {
         var url = $('.pagination .next_page').attr('href');
         if ( url && ($(window).scrollTop() > $(document).height() - $(window).height() - 80) ) {
-          $('.pagination').text("Fetching more posts");
+          $('.pagination').html($loading);
           $.getScript(url);
         }
       });
