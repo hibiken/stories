4a20d8b2a21c91ccac25e31ecf727ca9e2b47090
Ken Hibino <ken.hibino7@gmail.com>
2016-02-21 11:56:14 -0500

    Change CSS styles for post-panel in dashboard pages

--- app/controllers/tags_controller.rb
+++ app/controllers/tags_controller.rb
@@ -2,10 +2,6 @@ class TagsController < ApplicationController
   before_action :set_tag
   def show
     @dashboard = Dashboard.new(user: current_user, posts: tagged_posts, tag: @tag)
-    respond_to do |format|
-      format.html { render 'dashboards/show' }
-      format.js   { render 'dashboards/show' }
-    end
   end
 
   private
