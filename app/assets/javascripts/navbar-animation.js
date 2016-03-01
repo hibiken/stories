f1ff651975ed020a633dfbfd9a4a522e56519a1f
Ken Hibino <ken.hibino7@gmail.com>
2016-02-29 21:03:32 -0500

    Add animation to navbar

---
+++ app/assets/javascripts/navbar-animation.js
@@ -0,0 +1,23 @@
+var Navbar = {
+  animate: function() {
+    var lastScrollTop = 0;
+    var $navbar = $('[data-behavior="animated-navbar"]');
+    $(window).scroll(function(event) {
+      var st = $(this).scrollTop();
+      if (st > 500 && st > lastScrollTop) {
+        // downscroll event
+        $navbar.removeClass('is-inView');
+        $navbar.addClass('is-hidden');
+      } else {
+        // upscroll event
+        $navbar.removeClass('is-hidden');
+        $navbar.addClass('is-inView');
+      }
+      lastScrollTop = st;
+    });
+  }
+};
+
+$(document).ready( Navbar.animate );
+$(document).on( 'page:load', Navbar.animate );
+
