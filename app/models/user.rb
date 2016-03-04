d8f60b288d77ece4c7758af9a35ddcbf7a4d62ef
Ken Hibino <ken.hibino7@gmail.com>
2016-03-04 14:48:45 -0500

    Get rid of depreciated code

--- app/models/user.rb
+++ app/models/user.rb
@@ -65,7 +65,7 @@ class User < ActiveRecord::Base
 
   def self.new_with_session(params, session)
     if session["devise.user_attributes"]
-      new(session["devise.user_attributes"], without_protection: true) do |user|
+      new(session["devise.user_attributes"]) do |user|
         user.attributes = params
         user.valid?
       end
