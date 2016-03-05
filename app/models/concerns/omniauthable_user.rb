be362b0389e86a8340e451380f4bc1f48d74aebc
Ken Hibino <ken.hibino7@gmail.com>
2016-03-05 07:36:48 -0500

    Avoid email confilict between different Omniauth providers

--- app/models/concerns/omniauthable_user.rb
+++ app/models/concerns/omniauthable_user.rb
@@ -2,14 +2,16 @@ module OmniauthableUser
   extend ActiveSupport::Concern
 
   included do
-
+    # Add SecureRandom.hex to user's email to avaoid confilict between
+    # providers. If one user uses the same email for Facebook and Google,
+    # prepending this string will not cause "email has already been taken" error.
     def self.find_or_create_from_facebook_omniauth(auth)
       user = where(provider: auth.provider, uid: auth.uid).first_or_create
       unless auth.info.image.nil?
         user.remote_avatar_url = auth.info.image.gsub('http://','https://') + '?type=large'
       end
       user.update(
-        email: auth.info.email,
+        email: "#{SecureRandom.hex}#{auth.info.email}",
         password: Devise.friendly_token[0,20],
         username: auth.info.name
       )
@@ -24,7 +26,7 @@ module OmniauthableUser
       user.update(
         username: auth.info.name,
         password: Devise.friendly_token[0, 20],
-        email: auth.info.email || "#{auth.info.nickname}@mymediumclone.com"
+        email: "#{SecureRandom.hex}#{auth.info.nickname}@mymediumclone.com" # Twitter does not provide email
       )
       user
     end
@@ -34,7 +36,7 @@ module OmniauthableUser
       user.remote_avatar_url = auth.info.image
       user.update(
         username: auth.info.name,
-        email: auth.info.email,
+        email: "#{SecureRandom.hex}#{auth.info.email}",
         password: Devise.friendly_token[0, 20]
       )
       user
