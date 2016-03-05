ec5ac8c67b2e336f0606886c98f4979043eaa20f
Ken Hibino <ken.hibino7@gmail.com>
2016-03-04 21:08:32 -0500

    Extract omniauthable method from user to a separate module

---
+++ app/models/concerns/omniauthable_user.rb
@@ -0,0 +1,65 @@
+module OmniauthableUser
+  extend ActiveSupport::Concern
+
+  included do
+
+    def self.find_or_create_from_facebook_omniauth(auth)
+      user = where(provider: auth.provider, uid: auth.uid).first_or_create
+      unless auth.info.image.nil?
+        user.remote_avatar_url = auth.info.image.gsub('http://','https://') + '?type=large'
+      end
+      user.update(
+        email: auth.info.email,
+        password: Devise.friendly_token[0,20],
+        username: auth.info.name
+      )
+      user
+    end
+
+    def self.find_or_create_from_twitter_omniauth(auth)
+      user = where(provider: auth.provider, uid: auth.uid).first_or_create
+      unless auth.infor.image.nil?
+        user.remote_avatar_url = auth.info.image.gsub('http://', 'https://').gsub('_normal', '')
+      end
+      user.update(
+        username: auth.info.name,
+        password: Devise.friendly_token[0, 20],
+        email: auth.info.email || "#{auth.info.nickname}@mymediumclone.com"
+      )
+      user
+    end
+
+    def self.find_or_create_from_google_omniauth(auth)
+      user = where(provider: auth.provider, uid: auth.uid).first_or_create
+      user.remote_avatar_url = auth.info.image
+      user.update(
+        username: auth.info.name,
+        email: auth.info.email,
+        password: Devise.friendly_token[0, 20]
+      )
+      user
+    end
+
+    def self.new_with_session(params, session)
+      if session["devise.user_attributes"]
+        new(session["devise.user_attributes"]) do |user|
+          user.attributes = params
+          user.valid?
+        end
+      else
+        super
+      end
+    end
+
+  end
+
+
+  def password_required?
+    super && provider.blank?
+  end
+
+  def email_required?
+    super && provider.blank?
+  end
+
+end
--- app/models/user.rb
+++ app/models/user.rb
@@ -29,6 +29,7 @@ class User < ActiveRecord::Base
   mount_uploader :avatar, AvatarUploader
 
   include SearchableUser
+  include OmniauthableUser
 
   def add_like_to(likeable_obj)
     likes.where(likeable: likeable_obj).first_or_create
@@ -52,58 +53,6 @@ class User < ActiveRecord::Base
 
   def bookmarked?(bookmarkable_obj)
     send("bookmarked_#{downcased_class_name(bookmarkable_obj)}_ids").include?(bookmarkable_obj.id)
-  end
-
-  def self.find_or_create_from_facebook_omniauth(auth)
-    user = where(provider: auth.provider, uid: auth.uid).first_or_create
-    user.remote_avatar_url = auth.info.image.gsub('http://','https://') + '?type=large'
-    user.update(
-      email: auth.info.email,
-      password: Devise.friendly_token[0,20],
-      username: auth.info.name
-    )
-    user
-  end
-
-  def self.find_or_create_from_twitter_omniauth(auth)
-    user = where(provider: auth.provider, uid: auth.uid).first_or_create
-    user.remote_avatar_url = auth.info.image.gsub('http://', 'https://').gsub('_normal', '')
-    user.update(
-      username: auth.info.name,
-      password: Devise.friendly_token[0, 20],
-      email: auth.info.email || "#{auth.info.nickname}@mymediumclone.com"
-    )
-    user
-  end
-
-  def self.find_or_create_from_google_omniauth(auth)
-    user = where(provider: auth.provider, uid: auth.uid).first_or_create
-    user.remote_avatar_url = auth.info.image
-    user.update(
-      username: auth.info.name,
-      email: auth.info.email,
-      password: Devise.friendly_token[0, 20]
-    )
-    user
-  end
-
-  def self.new_with_session(params, session)
-    if session["devise.user_attributes"]
-      new(session["devise.user_attributes"]) do |user|
-        user.attributes = params
-        user.valid?
-      end
-    else
-      super
-    end
-  end
-
-  def password_required?
-    super && provider.blank?
-  end
-
-  def email_required?
-    super && provider.blank?
   end
 
   private
