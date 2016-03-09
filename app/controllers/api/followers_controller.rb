8926473f231e6b1f52d9bc6dd357630858200e23
Ken Hibino <ken.hibino7@gmail.com>
2016-03-08 21:40:37 -0500

    Adust the user overlay styles and pagination buttons position

--- app/controllers/api/followers_controller.rb
+++ app/controllers/api/followers_controller.rb
@@ -1,7 +1,7 @@
 class API::FollowersController < ApplicationController
   def index
     user = User.find(params[:user_id])
-    @followers = user.followers.paginate(page: params[:page], per_page: 7)
+    @followers = user.followers.paginate(page: params[:page], per_page: 6)
     @current_page = @followers.current_page
     @next_page = @followers.next_page
   end
--- app/controllers/api/following_controller.rb
+++ app/controllers/api/following_controller.rb
@@ -1,7 +1,7 @@
 class API::FollowingController < ApplicationController
   def index
     user = User.find(params[:user_id])
-    @following = user.following.paginate(page: params[:page], per_page: 7)
+    @following = user.following.paginate(page: params[:page], per_page: 6)
     @current_page = @following.current_page
     @next_page = @following.next_page
   end
