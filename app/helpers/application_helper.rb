af3bcae3a17419ae19f833c6b0fcc867fb6b262d
Ken Hibino <ken.hibino7@gmail.com>
2016-03-11 17:09:13 -0500

    Get rid of Redcarpet gem

--- app/helpers/application_helper.rb
+++ app/helpers/application_helper.rb
@@ -41,9 +41,4 @@ module ApplicationHelper
     link_to text, url, options
   end
 
-  def markdown(text)
-    @renderer ||= Redcarpet::Render::HTML.new(filter_html: false, hard_wrap: true) # Sets filter_html to false to use Medium Editor
-    @markdown_parser ||= Redcarpet::Markdown.new(@renderer, autolink: true, no_intra_emphasi: true)
-    @markdown_parser.render(text).html_safe
-  end
 end
