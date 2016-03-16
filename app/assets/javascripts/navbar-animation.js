7bfb4a99307143f677d53c6203d5124092f8f42a
Ken Hibino <ken.hibino7@gmail.com>
2016-03-16 19:07:47 -0400

    Add animation to metadata bar in posts#show page

--- app/assets/javascripts/navbar-animation.js
+++ app/assets/javascripts/navbar-animation.js
@@ -2,6 +2,7 @@ var Navbar = {
   animate: function() {
     var lastScrollTop = 0;
     var $navbar = $('[data-behavior="animated-navbar"]');
+    var $metadataBar = $('[data-behavior="animated-metadata"]');
     $(window).scroll(function(event) {
       var st = $(this).scrollTop();
       if (st > 500 && st > lastScrollTop) {
@@ -12,6 +13,14 @@ var Navbar = {
         // upscroll event
         $navbar.removeClass('is-hidden');
         $navbar.addClass('is-inView');
+      }
+
+      if (st > lastScrollTop) {
+        $metadataBar.removeClass('is-inView');
+        $metadataBar.addClass('is-hidden');
+      } else {
+        $metadataBar.removeClass('is-hidden');
+        $metadataBar.addClass('is-inView');
       }
       lastScrollTop = st;
     });
