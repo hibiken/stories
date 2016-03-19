2f20fc566127810d9729f8c59f379c59e44174f0
Ken Hibino <ken.hibino7@gmail.com>
2016-03-19 15:42:09 -0400

    Add followers query method for Tag model

--- app/models/tag.rb
+++ app/models/tag.rb
@@ -1,4 +1,7 @@
 class Tag < ActiveRecord::Base
   has_many :taggings, dependent: :destroy
   has_many :posts, through: :taggings
+
+  has_many :interests, dependent: :destroy
+  has_many :followers, through: :interests, source: :follower
 end
