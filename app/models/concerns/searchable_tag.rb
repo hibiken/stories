9b0b4cee1a40498f7c21da6e56ff0a304ef9ba3e
Ken Hibino <ken.hibino7@gmail.com>
2016-03-22 11:38:08 -0400

    Add SearchableTag module as a mixin

---
+++ app/models/concerns/searchable_tag.rb
@@ -0,0 +1,67 @@
+module SearchableTag
+  extend ActiveSupport::Concern
+
+    included do
+    include Elasticsearch::Model
+
+    # Sync up Elasticsearch with PostgreSQL.
+    after_commit :index_document, on: [:create, :update]
+    after_commit :delete_document, on: [:destroy]
+
+    settings INDEX_OPTIONS do
+      mappings dynamic: 'false' do
+        indexes :name, analyzer: 'autocomplete'
+      end
+    end
+
+    def self.search(term)
+      __elasticsearch__.search(
+        {
+          query: {
+            multi_match: {
+              query: term,
+              fields: ['name']
+            }
+          }
+        }
+      )
+    end
+  end
+
+  def as_indexed_json(options ={})
+    self.as_json({
+      only: [:name]
+    })
+  end
+
+  def index_document
+    ElasticsearchIndexJob.perform_later('index', 'Tag', self.id) if self.published?
+  end
+
+  def delete_document
+    ElasticsearchIndexJob.perform_later('delete', 'Tag', self.id) if self.published?
+  end
+
+  INDEX_OPTIONS =
+    { number_of_shards: 1, analysis: {
+    filter: {
+      "autocomplete_filter" => {
+        type: "edge_ngram",
+        min_gram: 1,
+        max_gram: 20
+      }
+    },
+    analyzer: {
+      "autocomplete" => {
+        type: "custom",
+        tokenizer: "standard",
+        filter: [
+          "lowercase",
+          "autocomplete_filter"
+        ]
+      }
+    }
+  }
+  }
+
+end
--- app/models/tag.rb
+++ app/models/tag.rb
@@ -4,4 +4,17 @@ class Tag < ActiveRecord::Base
 
   has_many :interests, dependent: :destroy
   has_many :followers, through: :interests, source: :follower
+
+  include SearchableTag
 end
+
+# Delete the previous posts index in Elasticsearch
+Tag.__elasticsearch__.client.indices.delete index: Tag.index_name rescue nil
+
+# Create the new index with the new mapping
+Tag.__elasticsearch__.client.indices.create \
+  index: Tag.index_name,
+  body: { settings: Tag.settings.to_hash, mappings: Tag.mappings.to_hash }
+
+# Index all post records from the DB to Elasticsearch
+Tag.import
