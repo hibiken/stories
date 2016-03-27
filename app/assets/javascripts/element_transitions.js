57b5a1e6b1f596747739b90ba8e54e7032f55f35
Ken Hibino <ken.hibino7@gmail.com>
2016-03-27 11:33:55 -0400

    Add animation to users#show users#edit page

--- app/assets/javascripts/element_transitions.js
+++ app/assets/javascripts/element_transitions.js
@@ -2,13 +2,13 @@ var ElementTransitions = {
   setup: function() {
     $(document).on('page:fetch.transition', function() {
       $('[data-animation="fadeInUp-fadeOutDown"]').addClass('animated fadeOutDown');
-      $('[data-animation="fadeInUp-fadeOutDown-small"]').addClass('animated fadeOutDown-small');
+      $('[data-animation="fadeInUp-fadeOutDown-slow"]').addClass('animated fadeOutDown-small');
       $('[data-animation="bounceInLeft-bounceOutLeft"]').addClass('animated bounceOutLeft');
     });
 
     $(document).on('page:change.transition', function() {
       $('[data-animation="fadeInUp-fadeOutDown"]').addClass('animated fadeInUp');
-      $('[data-animation="fadeInUp-fadeOutDown-small"]').addClass('animated fadeInUp-small');
+      $('[data-animation="fadeInUp-fadeOutDown-slow"]').addClass('animated fadeInUp-small');
       $('[data-animation="bounceInLeft-bounceOutLeft"]').addClass('animated bounceInLeft');
     });
   }
