3cf919ad642947db681daf93ed6c208537f2382a
Ken Hibino <ken.hibino7@gmail.com>
2016-03-29 21:39:51 -0400

    User Tag.first_or_create_with_name! method in Post#all_tags= and API::TagsController

--- app/controllers/api/tags_controller.rb
+++ app/controllers/api/tags_controller.rb
@@ -3,7 +3,7 @@ class API::TagsController < ApplicationController
 
   def create
     if params[:tag_name].strip.present?
-      tag = Tag.where(name: params[:tag_name].strip).first_or_create!
+      tag = Tag.first_or_create_with_name!(params[:tag_name])
       current_user.following_tags << tag unless current_user.following_tag?(tag)
     end
     render json: tag.to_json, status: 200
--- app/models/post.rb
+++ app/models/post.rb
@@ -65,7 +65,7 @@ class Post < ActiveRecord::Base
 
   def all_tags=(names)
     self.tags = names.split(",").map do |name|
-      Tag.where(name: name.strip).first_or_create!
+      Tag.first_or_create_with_name!(name)
     end
   end
 
