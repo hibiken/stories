6e547f88c0c8fdff8035872d568ae525ae7b54c1
Ken Hibino <ken.hibino7@gmail.com>
2016-04-08 16:20:25 -0400

    Add refresh link to follow suggestion component

--- app/assets/javascripts/components/follow_suggestions_container.es6.jsx
+++ app/assets/javascripts/components/follow_suggestions_container.es6.jsx
@@ -2,7 +2,10 @@ class FollowSuggestionsContainer extends React.Component {
   constructor(props) {
     super(props);
 
-    this.state = { users: [] };
+    this.state = { 
+      users: [],
+      activeUsers: []
+    };
   }
 
   componentWillMount() {
@@ -16,7 +19,11 @@ class FollowSuggestionsContainer extends React.Component {
       dataType: 'json',
       success: (data) => {
         console.log(data);
-        this.setState({ users: data });
+        const newActives = data.slice(0, 3)
+        this.setState({ 
+          activeUsers: newActives,
+          users: [ ...data.slice(3), ...newActives ]
+        });
       }
     });
   }
@@ -24,15 +31,43 @@ class FollowSuggestionsContainer extends React.Component {
   render () {
     return (
       <div className="follow-suggestions-container">
-        {this.renderSuggestions()}
+        <div className="suggestions-header">
+          <h4 className="small-heading">People to follow</h4>
+          <a className="refresh-link pull-right" onClick={this.refreshActiveUsers.bind(this)}>Refresh</a>
+        </div>
+        <div>
+          {this.renderSuggestions()}
+        </div>
       </div>
     );
   }
 
   renderSuggestions() {
-    return this.state.users.slice(0, 3).map(user => {
+    return this.state.activeUsers.map(user => {
       return <SuggestionItem key={user.id} {...user} />
     });
   }
+
+  refreshActiveUsers() {
+    const newActives = this.state.users.slice(0, 3);
+    this.setState({
+      activeUsers: newActives,
+      users: [ ...this.state.users.slice(3), ...newActives ]
+    });
+  }
+
+  // replaceUser(id) {
+  //   const filteredActives = this.state.activeUsers.filter(user => {
+  //     return user.id != id;
+  //   });
+  //   const filteredUsers = this.state.users.filter(user => {
+  //     return user.id != id;
+  //   });
+  //   this.setState({
+  //     users: [ ...filteredUsers.slice(1), filteredUsers[0] ],
+  //     activeUsers: [ ...filteredActives, filteredUsers[0] ]
+  //   });
+  // }
+
 }
 
