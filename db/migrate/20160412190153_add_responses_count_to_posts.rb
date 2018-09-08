2085571ff85d21b9e593987dc461596b3c9847a0
Miguel Michelson <miguelmichelson@gmail.com>
2018-09-08 05:02:44 -0300

    responses count without data manipulation

--- db/migrate/20160412190153_add_responses_count_to_posts.rb
+++ db/migrate/20160412190153_add_responses_count_to_posts.rb
@@ -2,11 +2,11 @@ class AddResponsesCountToPosts < ActiveRecord::Migration[4.2]
   def up
     add_column :posts, :responses_count, :integer, null: false, default: 0
     # reset cached counts for posts with responses
-    ids = Set.new
-    Post.replies.all.find_each { |r| ids << r.parent_id }
-    ids.each do |post_id|
-      Post.reset_counters(post_id, :responses)
-    end
+    #ids = Set.new
+    #Post.replies.all.find_each { |r| ids << r.parent_id }
+    #ids.each do |post_id|
+    #  Post.reset_counters(post_id, :responses)
+    #end
   end
 
   def down
