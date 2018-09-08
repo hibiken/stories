8d78cd253c53bb11ae4813bfc7a76da15eca7273
Miguel Michelson <miguelmichelson@gmail.com>
2018-09-08 02:11:53 -0300

    tag fix

--- app/javascript/components/TagFollowersList.js
+++ app/javascript/components/TagFollowersList.js
@@ -1,5 +1,6 @@
 import React from 'react';
 import ReactDOM from "react-dom";
+import PopoverLink from './PopoverLink'
 
 export default class TagFollowersList extends React.Component {
   constructor(props) {
