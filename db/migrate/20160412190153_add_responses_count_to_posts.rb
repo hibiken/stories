fa451579accf33f76bf71d8ed393eff01317f5dc
Miguel Michelson <miguelmichelson@gmail.com>
2018-09-08 04:48:11 -0300

    add migration response count

--- db/migrate/20160412190153_add_responses_count_to_posts.rb
+++ db/migrate/20160412190153_add_responses_count_to_posts.rb
@@ -3,7 +3,7 @@ class AddResponsesCountToPosts < ActiveRecord::Migration[4.2]
     add_column :posts, :responses_count, :integer, null: false, default: 0
     # reset cached counts for posts with responses
     ids = Set.new
-    Response.all.find_each { |r| ids << r.post_id }
+    Post.replies.all.find_each { |r| ids << r.parent_id }
     ids.each do |post_id|
       Post.reset_counters(post_id, :responses)
     end
